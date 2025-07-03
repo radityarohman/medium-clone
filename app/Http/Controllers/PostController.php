e08b66078c9cece5bf46fe69e250de1ae75664b6
Raditya Ananda Rohman <anandarohman2@gmail.com>
2025-07-03 23:32:23 +0700

    refactor(post): extract category tabs to a blade component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,10 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $categories = Category::get();
         $posts = Post::orderBy("created_at", "DESC")->simplePaginate(6);
         return view('dashboard', [
-            'categories' => $categories,
             'posts' => $posts
         ]);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,19 +3,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <ul
-                        class="flex flex-wrap justify-center text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-                        <li class="me-2">
-                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
-                                aria-current="page">All</a>
-                        </li>
-                        @foreach ($categories as $category)
-                            <li class="me-2">
-                                <a href="#"
-                                    class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100  ">{{ $category->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
+                    <x-category-tabs>
+                        No categories
+                    </x-category-tabs>
                 </div>
             </div>
 
