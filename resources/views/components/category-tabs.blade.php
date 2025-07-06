b19fd2985129f083aa23b3850026d5948bb3dcbd
Raditya Ananda Rohman <anandarohman2@gmail.com>
2025-07-06 21:58:10 +0700

    refactor(ui): replace standard button with reusable button component

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -1,6 +1,7 @@
 <ul class="flex flex-wrap justify-center text-sm font-medium text-center text-gray-500 dark:text-gray-400">
     <li class="me-2">
-        <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
+        <a href="#"
+            class="inline-block px-4 py-3 text-white rounded-lg active bg-gray-800 border border-transparent font-semibold text-xs hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
             aria-current="page">All</a>
     </li>
     @forelse ($categories as $category)
